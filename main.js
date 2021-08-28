106853d292ef792c21a54d5d54b85318f39ec337
nntdatkh <nntdat.kh@gmail.com>
2021-08-28 17:47:50 +0700

    finish the moving of the circle by keyboard

--- main.js
+++ main.js
@@ -18,3 +18,63 @@ const size = 30;
 drawCircle(x, y, size);
 
 // Add your code here
+let pressLeft, pressRight, pressUp, pressDown;
+
+canvas.addEventListener('keydown', (e) => {
+  switch(e.key){
+    case 'a':
+    case 'A':
+      pressLeft = true;
+      break;
+    case 'd':
+    case 'D':
+      pressRight = true;
+      break;
+    case 'w':
+    case 'W':
+      pressUp = true;
+      break;
+    case 's':
+    case 'S':
+      pressDown = true;
+    default:
+      break;
+  } 
+  moveAndDraw();
+})
+
+canvas.addEventListener('keyup', (e) => {
+  switch(e.key){
+    case 'a':
+    case 'A':
+      pressLeft = false;
+      break;
+    case 'd':
+    case 'D':
+      pressRight = false;
+      break;
+    case 'w':
+    case 'W':
+      pressUp = false;
+      break;
+    case 's':
+    case 'S':
+      pressDown = false;
+    default:
+      break;
+  } 
+})
+
+function moveAndDraw(){
+  if(pressLeft && x >= 55)
+    x -= 5;
+  else if(pressRight&& x <= canvas.width - 55)
+    x += 5;
+
+  if(pressUp && y >= 55)
+    y -= 5;
+  else if(pressDown && y <= canvas.height - 55)
+    y += 5;
+  
+  drawCircle(x, y, size);
+}
