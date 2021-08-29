8a7d664b5b221bd590d4de0b71fe0be605673953
nntdatkh <nntdat.kh@gmail.com>
2021-08-29 15:27:28 +0700

    first setup image gallery

--- main.js
+++ main.js
@@ -1,81 +1,13 @@
-let canvas = document.querySelector("canvas");
-let ctx = canvas.getContext("2d");
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
 
-function drawCircle(x, y, size) {
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
 
-  ctx.beginPath();
-  ctx.fillStyle = "black";
-  ctx.arc(x, y, size, 0, 2 * Math.PI);
-  ctx.fill();
-}
+/* Looping through images */
 
-let x = 50;
-let y = 50;
-const size = 30;
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+thumbBar.appendChild(newImage);
 
-drawCircle(x, y, size);
-
-// Add your code here
-let pressLeft, pressRight, pressUp, pressDown;
-
-canvas.addEventListener('keydown', (e) => {
-  switch(e.key){
-    case 'a':
-    case 'A':
-      pressLeft = true;
-      break;
-    case 'd':
-    case 'D':
-      pressRight = true;
-      break;
-    case 'w':
-    case 'W':
-      pressUp = true;
-      break;
-    case 's':
-    case 'S':
-      pressDown = true;
-    default:
-      break;
-  } 
-  e.defaultPrevented();
-  moveAndDraw();
-})
-
-canvas.addEventListener('keyup', (e) => {
-  switch(e.key){
-    case 'a':
-    case 'A':
-      pressLeft = false;
-      break;
-    case 'd':
-    case 'D':
-      pressRight = false;
-      break;
-    case 'w':
-    case 'W':
-      pressUp = false;
-      break;
-    case 's':
-    case 'S':
-      pressDown = false;
-    default:
-      break;
-  } 
-})
-
-function moveAndDraw(){
-  if(pressLeft && x >= 55)
-    x -= 5;
-  else if(pressRight&& x <= canvas.width - 55)
-    x += 5;
-
-  if(pressUp && y >= 55)
-    y -= 5;
-  else if(pressDown && y <= canvas.height - 55)
-    y += 5;
-  
-  drawCircle(x, y, size);
-}
+/* Wiring up the Darken/Lighten button */
